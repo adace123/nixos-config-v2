990701cdd7e48688dfbfde7bf23e1064e059f55d
aaron.feigenbaum <noreply@github.com>
2026-06-08 00:20:32 -0700

    feat(nixos): add nixos-raspberrypi cachix to common.nix

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -34,6 +34,12 @@
     auto-optimise-store = true;
     max-jobs = 4;
     cores = 4;
+    extra-substituters = [
+      "https://nixos-raspberrypi.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+    ];
   };
 
   # Enable NTP
