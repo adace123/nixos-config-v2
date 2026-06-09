8905d95ae6b9cb8211110d3298fc723e806d7b63
aaron.feigenbaum <noreply@github.com>
2026-06-08 22:54:55 -0700

    force neededForBoot on /boot/firmware instead of redefining filesystem

--- modules/nixos/coruscant/ssd.nix
+++ modules/nixos/coruscant/ssd.nix
@@ -9,12 +9,7 @@
   # to avoid building from source on the Pi
   boot.kernelPackages = lib.mkForce pkgs.linuxPackages;
 
-  # Ensure /boot/firmware is available during nixos-install for the
-  # RPi bootloader installer to copy firmware files (config.txt, DTBs, etc.)
-  fileSystems."/boot/firmware" = {
-    device = "/dev/disk/by-label/FIRMWARE";
-    fsType = "vfat";
-    options = [ "noatime" ];
-    neededForBoot = true;
-  };
+  # Ensure /boot/firmware is mounted early enough for the RPi bootloader
+  # installer to copy firmware files (config.txt, DTBs, etc.)
+  fileSystems."/boot/firmware".neededForBoot = lib.mkForce true;
 }
