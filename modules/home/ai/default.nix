346925583bb09cd13c90b123a0e650e862ace233
aaron.feigenbaum <noreply@github.com>
2026-06-08 17:42:58 -0700

    remove omp, add antigravity

--- modules/home/ai/default.nix
+++ modules/home/ai/default.nix
@@ -13,12 +13,12 @@
 
   home = {
     packages = with inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}; [
+      antigravity-cli
       claude-code
       ccstatusline
       ccusage
       hermes-agent
       pi
-      omp
     ];
 
     sessionVariables = {
