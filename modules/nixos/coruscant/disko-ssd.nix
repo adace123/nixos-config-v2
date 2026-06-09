75e60d0d80e13d3a57fa55a056a1640677643e3a
aaron.feigenbaum <noreply@github.com>
2026-06-08 22:37:43 -0700

    simplify disko config

--- modules/nixos/coruscant/disko-ssd.nix
+++ modules/nixos/coruscant/disko-ssd.nix
@@ -3,42 +3,29 @@
 
   disko.devices.disk.main = {
     type = "disk";
+    # Consider replacing with a stable path like: /dev/disk/by-id/usb-SSDO_Device_...
     device = "/dev/sda";
     content = {
       type = "gpt";
       partitions = {
-        firmware = {
-          size = "500M";
-          type = "0700";
-          attributes = [ 0 ];
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot/firmware";
-            mountOptions = [
-              "noatime"
-            ];
-          };
-        };
-        esp = {
-          size = "500M";
-          type = "EF00";
-          attributes = [ 2 ];
+        # Combined ESP and Firmware
+        boot = {
+          size = "1G";
+          type = "EF00"; # EFI System Partition
+          priority = 1; # Forces this to be partition 1
           content = {
             type = "filesystem";
             format = "vfat";
             mountpoint = "/boot";
             mountOptions = [
               "noatime"
-              "noauto"
-              "x-systemd.automount"
-              "x-systemd.idle-timeout=1min"
               "umask=0077"
             ];
           };
         };
         root = {
           size = "100%";
+          priority = 2;
           content = {
             type = "filesystem";
             format = "ext4";
