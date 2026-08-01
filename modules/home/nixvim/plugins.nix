65dad755d4bb4ca4ddd91d98a77393f99ba797c5
aaron.feigenbaum <noreply@github.com>
2026-07-31 18:24:40 -0700

    nixvim: defer cmdline completion until 3-char keywords

--- modules/home/nixvim/plugins.nix
+++ modules/home/nixvim/plugins.nix
@@ -239,6 +239,17 @@
                 };
               };
               buffer.score_offset = -7;
+              cmdline = {
+                __raw = ''
+                  {
+                    min_keyword_length = function(ctx)
+                      -- when typing a command, only show when the keyword is 3 characters or longer
+                      if ctx.mode == 'cmdline' and string.find(ctx.line, ' ') == nil then return 3 end
+                      return 0
+                    end,
+                  }
+                '';
+              };
             };
           };
           keymap = {
